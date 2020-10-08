485a309346cff8b0cf345dc6811d8c1f04157801
duointherain <duointherain@gmail.com>
2020-10-08 15:47:15 -0700

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
+// import theme from './theme';
 
 // import React from "react";
 import "./App.css";
-
+import calButton from './assets/calButton.png';
+import heartButton from './assets/heartButton.png';
+import shareButton from './assets/shareButton.png';
 function App() {
   const [imageOfTheDay, setImageOfTheDay ] = useState({});
   
@@ -22,10 +25,29 @@ function App() {
   return (
     <div className="App">
       <h1>Today's Photo</h1>
+     
       <p>
-       Our world is bigger than we're used to. There is endless space. Explore!  <span role="img" aria-label='go!'>🚀</span>!
-        <img src = {imageOfTheDay.url}></img>
+       Our world is bigger than we're used to. There is endless space. Explore!  <span role="img" aria-label='go!'>🚀</span>!</p>
+      
+        <img id="mainPic" src = {imageOfTheDay.url}></img>
+        
+     <p class="mainText"> 
+        <h3>About</h3>
+        <p>{imageOfTheDay.title}</p> 
+        <p>{imageOfTheDay.date}</p>
+        <p>{imageOfTheDay.explanation}</p>
+        <h6>copyright @ {imageOfTheDay.copyright}</h6>
       </p>
+    <nav>
+      <div class="mainButton">
+        <img src ={calButton}></img>  
+        </div>
+       <div class="mainButton"><img src = {heartButton}></img> </div>
+       <div class="mainButton"><img src ={shareButton}>
+          </img>
+       </div>
+    </nav>
+
     </div>
   );
 }
--- theme/index.js
+++ theme/index.js
@@ -14,4 +14,5 @@ export default {
       small: '4px',
       medium: '8px',
     }
+   
   }
